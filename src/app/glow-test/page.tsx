0637e9c12be5e7c5a4f276b21fc5b81108345d70
Marco Rodriguez <marcotbcreator@gmail.com>
2025-08-31 22:36:07 -0600

    refactor: remove unused GlowTestPage component and associated imports

--- src/app/glow-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GlowCardExample } from '@/components/ui/GlowCardExample';
-
-export default function GlowTestPage() {
-  return (
-    <div className="min-h-screen bg-background">
-      <GlowCardExample />
-    </div>
-  );
-}
